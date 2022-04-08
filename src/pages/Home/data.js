872139b7aafb1f111d9abe9b7d612a00a8c8dbef
Ernesto Junior <ernesto.sjunior@hotmail.com>
2022-04-08 19:07:56 -0300

    feat: include new categories

--- src/pages/Home/data.js
+++ src/pages/Home/data.js
@@ -2,5 +2,6 @@ export const categories = [
   { icon: 'pizza', title: 'Pizza' },
   { icon: 'soda', title: 'Soft Drinks' },
   { icon: 'shrimp', title: 'Seafood' },
+  { icon: 'iceCreams', title: 'Ice Creams' },
   { icon: 'bread', title: 'Breads' },
 ]
