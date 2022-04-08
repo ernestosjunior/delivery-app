7c156ed40acefe72c7da30e2cb297dbf8d94dcf8
Ernesto Junior <ernesto.sjunior@hotmail.com>
2022-04-08 20:23:56 -0300

    fix: optimize categorycard component

--- src/components/CategoryCard/index.js
+++ src/components/CategoryCard/index.js
@@ -3,15 +3,19 @@ import { Pressable } from 'react-native'
 import { Container, Title, StyledImage, Button, IconButton } from './styles'
 import { useRoot } from 'store'
 
-function CategoryCard({ icon, title, onClick }) {
+function CategoryCard({ icon, title, onClick, id }) {
   const { RootState } = useRoot()
 
-  const isSelected = RootState.selectedCategory === icon
+  const isSelected = RootState.selectedCategory === id
 
   return (
     <Pressable onPress={onClick}>
       <Container isSelected={isSelected}>
-        <RenderImage icon={icon} />
+        <StyledImage
+          source={{
+            uri: icon,
+          }}
+        />
         <Title>{title}</Title>
         <Button isSelected={isSelected}>
           <RenderIcon isSelected={isSelected} />
@@ -19,30 +23,6 @@ function CategoryCard({ icon, title, onClick }) {
       </Container>
     </Pressable>
   )
-}
-
-const RenderImage = ({ icon }) => {
-  if (icon === 'pizza') {
-    return <StyledImage source={require('../../assets/icons/pizza.png')} />
-  }
-
-  if (icon === 'shrimp') {
-    return <StyledImage source={require('../../assets/icons/shrimp.png')} />
-  }
-
-  if (icon === 'soda') {
-    return <StyledImage source={require('../../assets/icons/soda.png')} />
-  }
-
-  if (icon === 'bread') {
-    return <StyledImage source={require('../../assets/icons/bread.png')} />
-  }
-
-  if (icon === 'iceCreams') {
-    return <StyledImage source={require('../../assets/icons/iceCreams.png')} />
-  }
-
-  return null
 }
 
 const RenderIcon = ({ isSelected }) => {
