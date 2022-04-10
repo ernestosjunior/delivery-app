2061b0f78b6e01699ac619ccee994848e39caf6a
Ernesto Junior <ernesto.sjunior@hotmail.com>
2022-04-09 21:50:15 -0300

    feat: create service of home

---
+++ src/services/mockData.js
@@ -0,0 +1,44 @@
+export const categories = [
+  {
+    key: 'pizza',
+    title: 'Pizza',
+    icon: 'https://res.cloudinary.com/dcgzbotx7/image/upload/v1649459678/pizza-icon_1_vre0aw.png',
+  },
+  {
+    key: 'soda',
+    title: 'Soft Drinks',
+    icon: 'https://res.cloudinary.com/dcgzbotx7/image/upload/v1649459886/soda-icon_bfqd29.png',
+  },
+  {
+    key: 'shrimp',
+    title: 'Seafood',
+    icon: 'https://res.cloudinary.com/dcgzbotx7/image/upload/v1649459919/shrimp-icon_hpytkq.png',
+  },
+  {
+    key: 'iceCreams',
+    title: 'Ice Creams',
+    icon: 'https://res.cloudinary.com/dcgzbotx7/image/upload/v1649459989/iceCreams_rq4kbe.png',
+  },
+  {
+    key: 'bread',
+    title: 'Breads',
+    icon: 'https://res.cloudinary.com/dcgzbotx7/image/upload/v1649459988/bread_rjg2xx.png',
+  },
+]
+
+export const populars = [
+  {
+    id: 1,
+    name: 'Primavera Pizza',
+    evaluation: 5.0,
+    weight: 540,
+    img: 'https://res.cloudinary.com/dcgzbotx7/image/upload/v1649460275/pizza1_rnptms.png',
+  },
+  {
+    id: 2,
+    name: 'Primavera Pizza',
+    evaluation: 5.0,
+    weight: 540,
+    img: 'https://res.cloudinary.com/dcgzbotx7/image/upload/v1649460275/pizza1_rnptms.png',
+  },
+]
